47e75fadeec9d755587519d00f52a7e36584cc8c
Shibi404 <shibisbs123@gmail.com>
2021-09-28 08:13:36 +0530

    sdm660-common: SuperiorOS Bringup Signed-off-by: Shibi404 <shibisbs123@gmail.com>

--- sdm660.mk
+++ sdm660.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2020 The LineageOS Project
+# Copyright (C) 2021 The SuperiorOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
@@ -270,7 +270,7 @@ PRODUCT_PACKAGES += \
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-aosp
+    $(LOCAL_PATH)/overlay-sup
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
